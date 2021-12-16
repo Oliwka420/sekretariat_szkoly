b13cc00147a4361ad31ab647cb2fbedfb235d6e4
Visual Student <visualstudent@zschie.edu.pl>
2021-12-16 13:29:44 +0100

    zamienienie meni z combobox

--- sekretariat_szkoly/MainWindow.xaml.cs
+++ sekretariat_szkoly/MainWindow.xaml.cs
@@ -29,54 +29,6 @@ namespace sekretariat_szkoly
             InitializeComponent();
         }
         
-        private void uczen1a(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "1a";
-        }
-        private void uczen1b(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "1b";
-        }
-        private void uczen1c(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "1c";
-        }
-        private void uczen2a(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "2a";
-        }
-        private void uczen2b(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "2b";
-        }
-        private void uczen2c(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "2c";
-        }
-        private void uczen3a(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "3a";
-        }
-        private void uczen3b(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "3b";
-        }
-        private void uczen3c(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "3c";
-        }
-        private void uczen4a(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "4a";
-        }
-        private void uczen4b(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "4b";
-        }
-        private void uczen4c(object sender, RoutedEventArgs e)
-        {
-            meni.Header = "4c";
-        }
         
         private void dodanie_ucznia(object sender, RoutedEventArgs e)
         {
